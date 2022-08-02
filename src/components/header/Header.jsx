c0dc7a094f94d69a6cb53debd0d97aee25f85b0f
Nicolaswg <nigonbol123@gmail.com>
2022-08-02 16:36:57 -0400

    Call first header component

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,13 @@
 import React from 'react'
+import { SearchBar } from './searchbar/SearchBar'
 
 export const Header = () => {
   return (
-    <div>Header</div>
+    <div className="header_wrapper">
+      <h5 className="title">Lista de pokemon</h5>
+      <div className="hr_container">
+        <SearchBar />
+      </div>
+    </div>
   )
 }
