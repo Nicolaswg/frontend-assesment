f0f205cbdf729b75727b474a1f3351dc92b55314
Nicolaswg <nigonbol123@gmail.com>
2022-08-02 16:52:56 -0400

    Add searchbar structure

--- src/components/header/searchbar/SearchBar.jsx
+++ src/components/header/searchbar/SearchBar.jsx
@@ -2,6 +2,12 @@ import React from 'react'
 
 export const SearchBar = () => {
   return (
-    <div>SearchBar</div>
+    <div className="search_wrapper">
+      <div className='search_elements'>
+        <img src="" alt="" />
+        <input type="text" placeholder="buscar" />
+      </div>
+    </div>
   )
 }
+
