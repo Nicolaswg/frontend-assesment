0686cdf213ea2dd57664a06eb18574f54afd8a5e
Nicolaswg <nigonbol123@gmail.com>
2022-08-02 16:35:59 -0400

    Change main files format

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Header } from './components/header/Header'
+
+const App = () => {
+  return (
+    <div className="UI-wrapper">
+      <Header />
+    </div>
+  )
+}
+
+export default App;
