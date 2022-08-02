bbd305c9eacaf75c547f3a623b6f283c0e2c74f2
Nicolaswg <nigonbol123@gmail.com>
2022-08-02 18:00:43 -0400

    Implement searchbar compoenent design

--- src/components/header/searchbar/SearchBar.jsx
+++ src/components/header/searchbar/SearchBar.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import { FiSearch } from 'react-icons/fi'
+import './search_bar.scss'
 
 export const SearchBar = () => {
   return (
     <div className="search_wrapper">
-      <div className='search_elements'>
-        <img src="" alt="" />
+      <div className="search_bar">
+        <FiSearch className="search_icon" />
         <input type="text" placeholder="buscar" />
       </div>
     </div>
