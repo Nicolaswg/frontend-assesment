46a422cd97af20f6446908dfe0f7c2ac89a8a555
Nicolaswg <nigonbol123@gmail.com>
2022-08-02 17:19:10 -0400

    include reac-icons and basic component placement on virtualDome

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import { Header } from './components/header/Header'
+import "./app.scss"
 
 const App = () => {
   return (
-    <div className="UI-wrapper">
+    <div className="components_wrapper">
       <Header />
     </div>
   )
