76b5739250d570df819ed69cc4800387cb604648
Nicolaswg <nigonbol123@gmail.com>
2022-08-03 18:54:32 -0400

    Implement onclick event for displaying modal

--- src/components/header/add_button/AddButton.jsx
+++ src/components/header/add_button/AddButton.jsx
@@ -1,15 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { BsPlusLg } from 'react-icons/bs'
 
 import './add_button.scss'
 
-const AddButton = () => {
-    return (
-        <div className="btn_wrapper" data-testid="add_button">
-            <BsPlusLg className="plus_icon" />
-            <p>Nuevo</p>
-        </div>
-    )
+const AddButton = ({ callback }) => {
+  const [innerOpen, setInnerOpen] = useState(false);
+
+  const handleOnclick = (e) => {
+    callback(innerOpen)
+    setInnerOpen(!innerOpen)
+  }
+
+  return (
+    <div className="btn_wrapper" data-testid="add_button" onClick={handleOnclick}>
+      <BsPlusLg className="plus_icon" />
+      <p>Nuevo</p>
+    </div>
+  )
 }
 
 export default AddButton
